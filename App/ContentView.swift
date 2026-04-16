06cb3625842f7ebb92b10d8bbb13d0acb2468d67
Liz77 <108732510+Pip7747@users.noreply.github.com>
2026-04-16 16:14:26 +1000

    Update ContentView.swift

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -2,17 +2,6 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            GameContainerView()
-                .tabItem {
-                    Label("Retro Racer", systemImage: "car.fill")
-                }
-
-            LaunchPadView()
-                .tabItem {
-                    Label("LaunchPad", systemImage: "music.note.list")
-                }
-        }
-        .tint(.yellow)
+        LaunchPadView()
     }
 }
